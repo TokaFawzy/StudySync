1446b5260cbf478788302929fd49acd4c22c7558
Toka Fawzy <totad578@gmail.com>
2026-04-19 14:41:46 +0200

    Add Register Page

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  validEmail = /^[a-zA-Z0-9._%+-]{3,}@ics\.tanta\.edu\.eg$/;
+  registerForm=new FormGroup({
+    name:new FormControl('',[Validators.required,Validators.minLength(8)]),
+    userName:new FormControl('',[Validators.required,Validators.minLength(4)]),
+    number:new FormControl(''),
+    role:new FormControl('',[Validators.required]),
+    department:new FormControl('',[Validators.required]),
+    level:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.pattern(this.validEmail)]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8)]),
+    cardImg:new FormControl(null,[Validators.required]),
+  })
+  constructor(){
+    this.registerForm.get('role')?.valueChanges.subscribe((value)=>{
+      const controleNumber=this.registerForm.get('number');
+      if(value=='student'){
+        controleNumber?.setValidators([Validators.required,Validators.minLength(7)]);
+      }else{
+        controleNumber?.clearValidators();
+      }
+      controleNumber?.updateValueAndValidity();
+    });
+  }
+  imagePreview: string | null = null;
+  onFileSelected(event: any) {
+    const file=event.target.files[0];
+    if(file){
+      this.registerForm.patchValue({cardImg:file});
+      this.registerForm.get('cardImg')?.markAsTouched();
+      this.registerForm.get('cardImg')?.updateValueAndValidity();
+      const reader=new FileReader();
+      reader.onload=()=>{
+        this.imagePreview=reader.result as string;
+      }
+      reader.readAsDataURL(file);
+    }
+  }
 
 }
